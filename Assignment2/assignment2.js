6fdf53f6ec49364860f57a71654ade041a0ee31f
DatLam25 <d4nnyl4m@gmail.com>
2024-02-14 18:11:17 -0700

    Completed some requirements for A2

--- Assignment2/assignment2.js
+++ Assignment2/assignment2.js
@@ -1,3 +1,7 @@
+//TODO: Usage of bind, call and apply
+
+// User object: Has username and keeps track of score history
+//TODO: Keep track of user score (the game reset if user get 3 wrong answers?)
 function User(name){
     this.username = name;
     this.scoreHistory = [];
@@ -5,12 +9,9 @@ function User(name){
     this.addScore = function(score){
         this.scoreHistory.push(score);
     };
-
-    this.getScores = function(){ 
-        return this.score;
-    };
 };
 
+// Question object: Has question, options, and answer
 function Question(question, options, answer){
     this.question = question;
     this.options = options;
@@ -21,26 +22,80 @@ function Question(question, options, answer){
     };
 };
 
-const Quiz = {
-    questions: [],
-    currentScore: 0,
-    currentQuestion: 0,
-    started: false,
 
-    startQuiz(){
+// Quiz object
+const quiz = {
+    currentScore: 0,
+    storedQuestion: [],
+    currentQuestion: null,
+    questionNumber: 0,
+    user: null,
+    titleElement: null,
+
+    async startQuiz(){
         this.started = true;
         this.currentScore = 0;
-        this.currentQuestion = 0;
+        await this.fetchQuestion("easy");
+        this.user = new User(document.getElementById('name').value);
+        this.titleElement = document.getElementById('title');
+        document.getElementById("start").remove();
+        document.getElementById("quiz").style.display = "block";
+        this.presentQuestion();
+        
+    },
+
+    async fetchQuestion (difficulty){
+        // Fetching 10 questions at a time to avoid timeout
+        let apiURL = `https://opentdb.com/api.php?amount=10&difficulty=${difficulty}&type=multiple`;
+        await fetch(apiURL).then(response => response.json()).then(data => {
+            if(data.response_code == 5){
+                //TODO: Error Handling (API limit reached: There is a 5 second timeout after each request)
+                console.log("API limit reached");
+                return;
+            }
+            data.results.forEach((question) => {
+                let options = question.incorrect_answers.concat(question.correct_answer);
+                options.sort(() => Math.random() - 0.5);
+                this.storedQuestion.push(new Question(question.question, options, question.correct_answer));
+            });
+        });
     },
 
     presentQuestion(){
-        return this.questions[this.currentQuestion];
+        const questionNumber = document.getElementById('questionNumber');
+        const questionElement = document.getElementById('question');
+        const optionsElement = document.getElementById('choices');
+        this.titleElement.innerHTML = `${this.user.username}: ${this.currentScore}`;
+        questionNumber.innerHTML = `Question ${this.questionNumber + 1}`;
+        this.currentQuestion = this.storedQuestion.pop();
+        questionElement.innerHTML = this.currentQuestion.question;
+        
+
+        optionsElement.innerHTML = '';
+        this.currentQuestion.options.forEach((option) => {
+            const optionElement = document.createElement('button');
+            optionElement.innerHTML = option;
+            optionElement.addEventListener('click', () => {
+                this.answerQuestion(option);
+            });
+            optionsElement.appendChild(optionElement);
+        });
     },
 
-    answerQuestion(answer){
-        if(this.questions[this.currentQuestion].checkAnswer(answer)){
+    async answerQuestion(answer){
+        document.getElementById('choices').innerHTML = '';
+        if(this.currentQuestion.checkAnswer(answer)){
             this.currentScore++;
         }
-        this.currentQuestion++;
+        this.questionNumber++;
+        if(this.storedQuestion.length <= 0){
+            console.log("fetching new question")
+            await this.fetchQuestion("easy");
+            this.presentQuestion();
+        }
+        else{
+            console.log("Presenting new question")
+            this.presentQuestion();
+        }
     },
 };
