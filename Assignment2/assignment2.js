63b4ce55053cb6973f1720bd30de38edb7cabf74
Amanda Nguyen <77892730+AmandaNguye@users.noreply.github.com>
2024-02-15 08:19:22 -0700

    Add question difficulty adjustment, answer display and button responsiveness, and API call reattempt on error

--- Assignment2/assignment2.js
+++ Assignment2/assignment2.js
@@ -1,102 +1,135 @@
 //TODO: Usage of bind, call and apply
 
 // User object: Has username and keeps track of score history
-//TODO: Keep track of user score (the game reset if user get 3 wrong answers?)
-function User(name){
-    this.username = name;
-    this.scoreHistory = [];
+function User(name) {
+  this.username = name;
+  this.scoreHistory = [];
 
-    this.addScore = function(score){
-        this.scoreHistory.push(score);
-    };
-};
+  this.addScore = function (score) {
+    this.scoreHistory.push(score);
+  };
+}
 
 // Question object: Has question, options, and answer
-function Question(question, options, answer){
-    this.question = question;
-    this.options = options;
-    this.answer = answer;
+function Question(question, options, answer) {
+  this.question = question;
+  this.options = options;
+  this.answer = answer;
 
-    this.checkAnswer = function(userAnswer){
-        return this.answer === userAnswer;
-    };
-};
+  this.checkAnswer = function (userAnswer) {
+    return this.answer === userAnswer;
+  };
+}
 
+// pairing difficulty to a word
+let difficultyWord = { 1: "Easy", 2: "Medium", 3: "Hard" };
 
 // Quiz object
 const quiz = {
-    currentScore: 0,
-    storedQuestion: [],
-    currentQuestion: null,
-    questionNumber: 0,
-    user: null,
-    titleElement: null,
+  currentScore: 0,
+  storedQuestion: [],
+  currentQuestion: null,
+  questionNumber: 0,
+  user: null,
+  titleElement: null,
+  scoreElement: null,
+  difficulty: 1,
 
-    async startQuiz(){
-        this.started = true;
-        this.currentScore = 0;
-        await this.fetchQuestion("easy");
-        this.user = new User(document.getElementById('name').value);
-        this.titleElement = document.getElementById('title');
-        document.getElementById("start").remove();
-        document.getElementById("quiz").style.display = "block";
-        this.presentQuestion();
-        
-    },
+  async startQuiz() {
+    this.started = true;
+    this.currentScore = 0;
+    await this.fetchQuestion("easy");
+    this.user = new User(document.getElementById("name").value);
+    this.titleElement = document.getElementById("title");
+    this.scoreElement = document.getElementById("score");
+    document.getElementById("start").remove();
+    document.getElementById("quiz").style.display = "block";
+    this.presentQuestion();
+  },
 
-    //TODO: Adjust difficulty based on user score
-    async fetchQuestion (difficulty){
-        // Fetching 10 questions at a time to avoid timeout
-        let apiURL = `https://opentdb.com/api.php?amount=10&difficulty=${difficulty}&type=multiple`;
-        await fetch(apiURL).then(response => response.json()).then(data => {
-            if(data.response_code == 5){
-                //TODO: Error Handling (API limit reached: There is a 5 second timeout after each request)
-                console.log("API limit reached");
-                return;
-            }
-            data.results.forEach((question) => {
-                let options = question.incorrect_answers.concat(question.correct_answer);
-                options.sort(() => Math.random() - 0.5);
-                this.storedQuestion.push(new Question(question.question, options, question.correct_answer));
-            });
+  async fetchQuestion(difficulty) {
+    // let apiURL = `https://opentdb.com/api.php?amount=10&difficulty=${difficulty}&type=multiple`;
+    let apiURL = `https://opentdb.com/api.php?amount=1&difficulty=${difficulty}&type=multiple`;
+    await fetch(apiURL)
+      .then(async (response) => {
+        //if error, then recursively recall the function after 5 seconds
+        if (response.status == 429) {
+          console.log("API limit reached. Retrying after 5 seconds...");
+          await new Promise((resolve) => setTimeout(resolve, 2500)); // Wait for additional 2.5 seconds for 5 seconds total (after fetching answer)
+          return this.fetchQuestion(difficulty); // Retry the request
+        }
+        return response.json();
+      })
+      .then((data) => {
+        //if no data, then return since we are recursively calling the function on error
+        if (!data || data.response_code == 5) {
+          return;
+        }
+        data.results.forEach((question) => {
+          let options = question.incorrect_answers.concat(
+            question.correct_answer
+          );
+          options.sort(() => Math.random() - 0.5);
+          this.storedQuestion.push(
+            new Question(question.question, options, question.correct_answer)
+          );
         });
-    },
+      });
+  },
 
-    presentQuestion(){
-        const questionNumber = document.getElementById('questionNumber');
-        const questionElement = document.getElementById('question');
-        const optionsElement = document.getElementById('choices');
-        this.titleElement.innerHTML = `${this.user.username}: ${this.currentScore}`;
-        questionNumber.innerHTML = `Question ${this.questionNumber + 1}`;
-        this.currentQuestion = this.storedQuestion.pop();
-        questionElement.innerHTML = this.currentQuestion.question;
-        
-
-        optionsElement.innerHTML = '';
-        this.currentQuestion.options.forEach((option) => {
-            const optionElement = document.createElement('button');
-            optionElement.innerHTML = option;
-            optionElement.addEventListener('click', () => {
-                this.answerQuestion(option);
-            });
-            optionsElement.appendChild(optionElement);
+  presentQuestion() {
+    document.getElementById("answer").innerHTML = "";
+    const questionNumber = document.getElementById("questionNumber");
+    const questionElement = document.getElementById("question");
+    const optionsElement = document.getElementById("choices");
+    this.titleElement.innerHTML = `${this.user.username}`;
+    this.scoreElement.innerHTML = `${this.currentScore}/${this.questionNumber}`;
+    questionNumber.innerHTML = `Question ${this.questionNumber + 1} (${
+      difficultyWord[this.difficulty]
+    })`;
+    this.currentQuestion = this.storedQuestion.pop();
+    questionElement.innerHTML = this.currentQuestion.question;
+    optionsElement.innerHTML = "";
+    this.currentQuestion.options.forEach((option) => {
+      const optionElement = document.createElement("button");
+      optionElement.innerHTML = option;
+      optionElement.addEventListener("click", () => {
+        this.answerQuestion(option);
+        // disable buttons after one is clicked
+        document.querySelectorAll("button").forEach((button) => {
+          button.disabled = true;
+          button.classList.add("disabled");
+          if (
+            button.innerHTML === option &&
+            this.currentQuestion.checkAnswer(option)
+          ) {
+            button.classList.add("true");
+          } else if (button.innerHTML === option) {
+            button.classList.add("false");
+          }
         });
-    },
+      });
+      optionsElement.appendChild(optionElement);
+    });
+  },
 
-    async answerQuestion(answer){
-        document.getElementById('choices').innerHTML = '';
-        if(this.currentQuestion.checkAnswer(answer)){
-            this.currentScore++;
-        }
-        this.questionNumber++;
-        if(this.storedQuestion.length <= 0){
-            console.log("fetching new question")
-            await this.fetchQuestion("easy");
-            this.presentQuestion();
-        }
-        else{
-            console.log("Presenting new question")
-            this.presentQuestion();
-        }
-    },
+  async answerQuestion(answer) {
+    // document.getElementById("choices").innerHTML = "";
+    document.getElementById(
+      "answer"
+    ).innerHTML = `Answer: ${this.currentQuestion.answer}`;
+    let correct = this.currentQuestion.checkAnswer(answer);
+    // increase or decrease difficulty depending on correctness
+    if (correct) {
+      this.currentScore++;
+      this.difficulty = this.difficulty < 3 ? this.difficulty + 1 : 3;
+    } else {
+      this.difficulty = this.difficulty > 1 ? this.difficulty - 1 : 1;
+    }
+    this.questionNumber++;
+    console.log("fetching new question");
+    await new Promise((resolve) => setTimeout(resolve, 2500)); // Wait for 2.5 seconds
+    await this.fetchQuestion(difficultyWord[this.difficulty].toLowerCase());
+    this.presentQuestion();
+  },
 };
