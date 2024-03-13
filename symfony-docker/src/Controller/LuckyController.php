7fdfd26f2fdc20ab4c0ed548717beb21c2686723
DatLam25 <d4nnyl4m@gmail.com>
2024-03-13 17:03:20 -0600

    Add Symfony Docker from Part 1 of tutorial

---
+++ symfony-docker/src/Controller/LuckyController.php
@@ -0,0 +1,33 @@
+<?php
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController; 
+
+class LuckyController extends AbstractController
+{
+    #[Route('/lucky/number', name: 'app_lucky_number')]
+    public function number(): Response
+    {
+        $number = random_int(0, 100);
+
+        return $this->render('lucky/number.html.twig', [
+            'number' => $number,
+            'title' => 'Lucky Number',
+            'message' => 'Your lucky number is:'
+        ]);
+    }
+
+    #[Route('/lucky/number/{max}', name: 'app_lucky_number_max')]
+    public function numberMax($max): Response
+    {
+        $number = random_int(0, $max);
+
+        return $this->render('lucky/number.html.twig', [
+            'number' => $number,
+            'title' => 'Lucky Number',
+            'message' => 'Hey<br>lucky number is:'
+        ]);
+    }
+}
